67e865ec57611b63601f6b10ac358bd384dd391c
Ethan Stacy <ethanstacy02@hotmail.com>
2024-10-31 20:47:26 -0600

    Get input path from args

--- src/Program.cs
+++ src/Program.cs
@@ -53,42 +53,49 @@ namespace FloodFill
             return idMap;
         }
 
-        public static Result<bool[,], IOException> ReadMapFromFile(string inPath)
+        public static Result<bool[,], Exception> ReadMapFromFile(string inPath)
         {
             List<bool[]> map = [];
             Optional<char> firstChar = new();
             Optional<char> secondChar = new();
-            var lines = File.ReadLines(inPath);
-            foreach (string line in lines)
+            try
             {
-                if (map.Count > 0 && line.Length != map[0].Length)
-                    return new Result<bool[,], IOException>(new IOException("Line lengths are not equal"));
-                bool[] lineValues = new bool[line.Length];
-                for (int i = 0; i < line.Length; i++)
+                var lines = File.ReadLines(inPath);
+                foreach (string line in lines)
                 {
-                    char c = line[i];
-                    if (firstChar.IsSome())
+                    if (map.Count > 0 && line.Length != map[0].Length)
+                        return new Result<bool[,], Exception>(new IOException("Line lengths are not equal"));
+                    bool[] lineValues = new bool[line.Length];
+                    for (int i = 0; i < line.Length; i++)
                     {
-                        if (secondChar.IsSome())
+                        char c = line[i];
+                        if (firstChar.IsSome())
                         {
-                            if (c != secondChar.Value && c != firstChar.Value)
+                            if (secondChar.IsSome())
                             {
-                                return new Result<bool[,], IOException>(new IOException($"More than two distinct characters in input file!\nFirst char is {firstChar}\nSecond char is {secondChar}\nThird char is \"{c}\")"));
+                                if (c != secondChar.Value && c != firstChar.Value)
+                                {
+                                    return new Result<bool[,], Exception>(new IOException($"More than two distinct characters in input file!\nFirst char is {firstChar}\nSecond char is {secondChar}\nThird char is \"{c}\")"));
+                                }
+                            }
+                            else if (c != firstChar.Value)
+                            {
+                                secondChar.Set(c);
                             }
                         }
-                        else if (c != firstChar.Value)
+                        else
                         {
-                            secondChar.Set(c);
+                            firstChar.Set(c);
                         }
-                    }
-                    else
-                    {
-                        firstChar.Set(c);
-                    }
 
-                    lineValues[i] = c == firstChar.Value;
+                        lineValues[i] = c == firstChar.Value;
+                    }
+                    map.Add(lineValues);
                 }
-                map.Add(lineValues);
+            }
+            catch (Exception e)
+            {
+                return new Result<bool[,], Exception>(e);
             }
 
             bool[,] arrayMap = new bool[map.Count, map[0].Length];
@@ -101,7 +108,7 @@ namespace FloodFill
                 }
             }
 
-            return new Result<bool[,], IOException>(arrayMap);
+            return new Result<bool[,], Exception>(arrayMap);
         }
 
         public static Result<Empty, Exception> WriteMapToFile(string outPath, int[,] outputMap)
@@ -135,19 +142,24 @@ namespace FloodFill
 
     class Program
     {
-        static int Main()
+        static int Main(string[] args)
         {
-            var readResult = FloodFill.ReadMapFromFile("demo_files/zero_one.ffin");
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("First argument should be input file path");
+                return -1;
+            }
+            var readResult = FloodFill.ReadMapFromFile(args[0]);
             if (readResult.IsError)
             {
-                Console.Error.WriteLine($"Could not read input map with error {readResult.Error}");
+                Console.Error.WriteLine($"Could not read input map with error: {readResult.Error}");
                 return -1;
             }
             int[,] outputMap = FloodFill.SolveFloodFill(readResult.Value);
-            var writeResult = FloodFill.WriteMapToFile("demo_files/zero_one.ffout", outputMap);
+            var writeResult = FloodFill.WriteMapToFile(Path.ChangeExtension(args[0], "ffout"), outputMap);
             if (writeResult.IsError)
             {
-                Console.Error.WriteLine($"Could not write output map with error {readResult.Error}");
+                Console.Error.WriteLine($"Could not write output map with error: {readResult.Error}");
                 return -1;
             }
 
